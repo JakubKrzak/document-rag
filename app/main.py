f746249b08f0ec23c39919146a9e28b29e2e9a09
JakubKrzak <jakub.krzak.main@gmail.com>
2026-04-12 13:01:17 +0200

    add new router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import files, db_health
+from .routers import db_health, files_endpoint
 
 
 app = FastAPI()
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(db_health.router)
-app.include_router(files.router)
+app.include_router(files_endpoint.router)
 
 @app.get("/", tags=['status'])
 def server_status():
