be15207b0fbe020566d0693606b9ceae77ea4ffd
JakubKrzak <jakub.krzak.main@gmail.com>
2026-04-15 23:49:19 +0200

    create 2 func for ingestion file, first for parsed content and return docling object, second for save markdown file on disc

---
+++ app/services/file_parsed_content.py
@@ -0,0 +1,27 @@
+
+from ast import parse
+
+from docling.document_converter import DocumentConverter
+import asyncio
+
+from fastapi import Path
+from docling.document_converter import DocumentConverter
+
+
+async def parsed_file_content(file_path: Path):
+    file_converter = DocumentConverter()
+    result = await asyncio.to_thread(file_converter.convert, file_path)
+    doc = result.document
+    return doc
+
+async def save_parsed_file_on_disc(file_object, parsed_file: object):
+    file_path = f"parsed_disc/{parsed_file.name}.md"
+    
+    def _write():
+        md = parsed_file.export_to_markdown()
+        with open(file_path, "w") as f:
+            f.write(md)
+    
+    await asyncio.to_thread(_write)
+
+    return file_path
