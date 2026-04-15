3773287f32d6b2f00bf49ee9e8fa5f5af5aa6d5a
JakubKrzak <jakub.krzak.main@gmail.com>
2026-04-15 23:46:55 +0200

    common query to search in db

---
+++ app/services/file_services.py
@@ -0,0 +1,15 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from database import models
+
+from ..schemas import schemas_file
+
+def find_file_by_id(file_id: str, db: Session):
+    return db.query(models.File).filter(models.File.id == file_id).first()
+
+def find_file_by_name(file_name: str, db: Session):
+    return db.query(models.File).filter(models.File.file_name == file_name).all()
+
+def get_all_files(db: Session) -> list[schemas_file.FileResponse]:
+    return db.query(models.File).all()
+    
