aaab818d730b53a9fe0f5ffef809106f55bae184
JakubKrzak <jakub.krzak.main@gmail.com>
2026-04-19 23:26:46 +0200

    create function for checking current file status

--- app/services/file_services.py
+++ app/services/file_services.py
@@ -1,3 +1,5 @@
+import enum
+
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from database import models
@@ -12,4 +14,11 @@ def find_file_by_name(file_name: str, db: Session):
 
 def get_all_files(db: Session) -> list[schemas_file.FileResponse]:
     return db.query(models.File).all()
-    
+
+def check_file_status(id: str, status: models.FileStatus, db: Session):
+    file = db.query(models.File).filter(models.File.id == id).first()
+
+    if file is None:
+        return False
+
+    return file.status == status
