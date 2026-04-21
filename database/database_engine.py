18f0d9341105f95c762d8ba5dd621976e923d760
JakubKrzak <jakub.krzak.main@gmail.com>
2026-04-21 19:13:06 +0200

    delete useles import

--- database/database_engine.py
+++ database/database_engine.py
@@ -1,14 +1,14 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config.settings import settings
 
 SQLALCHEMY_DATABASE_URL = f'postgresql+asyncpg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
 
 Base = declarative_base()
 async def get_db():
