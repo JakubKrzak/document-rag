7ea3c0812ba2e008eb7940b4c967602c884091d4
JakubKrzak <jakub.krzak.main@gmail.com>
2026-04-24 22:58:23 +0200

    add new functions

--- app/services/shared/repository.py
+++ app/services/shared/repository.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from database import models
+from schemas import schemas_file
 
 
 async def find_file_by_id(file_id: str, db: AsyncSession):
@@ -32,3 +33,19 @@ async def update_file_status(file_id: str, status: models.FileStatus, db: AsyncS
     await db.commit()
     await db.refresh(file)
     return True
+
+async def find_file_by_name(file_name: str, db: AsyncSession):
+    result = await db.execute(select(models.File).where(models.File.file_name == file_name))
+    return result.scalars().all()
+
+async def get_all_files(db: AsyncSession) -> list[schemas_file.FileResponse]:
+    result = await db.execute(select(models.File))
+    return result.scalars().all()
+
+async def check_file_status(file_id: str, status: models.FileStatus, db: AsyncSession):
+    file = await find_file_by_id(file_id=file_id, db=db)
+
+    if file is None:
+        return None
+
+    return file.status == status 
