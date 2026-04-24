c02d7c38636eb4e681adf4931a0e6643bb0c5bae
JakubKrzak <jakub.krzak.main@gmail.com>
2026-04-24 22:57:21 +0200

    add all columns to schemas

--- app/schemas/schemas_file.py
+++ app/schemas/schemas_file.py
@@ -1,6 +1,8 @@
 import datetime
 from uuid import UUID
 from pydantic import BaseModel
+from database.models import FileStatus
+from typing import Optional
 
 class FileResponse(BaseModel):
     id: UUID
@@ -8,8 +10,15 @@ class FileResponse(BaseModel):
     hashed_content: str
     file_size: int
     file_type: str
+    parsed_file_path: Optional[str]=None
     file_path: str
+    pages: Optional[int]=None
     uploaded_at: datetime
+    chunks_path: Optional[str]=None
+    chunks_number: Optional[int]=None
+    embed_path: Optional[str]=None
+    vectors_dim: Optional[int]=None
+    status: FileStatus
 
     class Config:
         from_attributes = True
