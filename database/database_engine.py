cbb9f5dc435961f338d6b26eec8945f2e9b17a39
JakubKrzak <jakub.krzak.main@gmail.com>
2026-05-07 13:15:14 +0200

    engine for qdrant db

--- database/database_engine.py
+++ database/database_engine.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config.settings import settings
 
+
 SQLALCHEMY_DATABASE_URL = f'postgresql+asyncpg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
