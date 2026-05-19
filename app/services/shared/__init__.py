b56f9884cb7b26bf66bc04247d599650ce9c63f5
JakubKrzak <jakub.krzak.main@gmail.com>
2026-05-19 13:59:15 +0200

    errors

--- app/services/shared/__init__.py
+++ app/services/shared/__init__.py
@@ -1,1 +1,3 @@
 from .repository import find_file_by_id, update_file_status, find_file_by_name, get_all_files, check_file_status                 
+
+from .errors import DocumentNotFoundError, PathNotFoundError, AddFileToDataBaseError, ChunksEmptyError, EmptyVectorsError, WrongVectorsDimError, EmptyPointsError, VectrDatabaseError
