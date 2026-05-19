8b43e823e534bd1e4f16bc65b24d6a494f49380d
JakubKrzak <jakub.krzak.main@gmail.com>
2026-05-19 13:58:33 +0200

    add error handlers fir parser.py

--- app/services/parsing/parser.py
+++ app/services/parsing/parser.py
@@ -2,8 +2,15 @@ from docling.document_converter import DocumentConverter
 import asyncio
 from pathlib import Path
 
+from app.services.shared.errors import PathNotFoundError
+
 async def parsed_file_content(file_path: Path):
+    if not file_path:
+        raise PathNotFoundError(path=file_path)
+    
+    
     file_converter = DocumentConverter()
     result = await asyncio.to_thread(file_converter.convert, file_path)
     doc = result.document
+    
     return doc
